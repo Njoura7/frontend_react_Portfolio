00066847356d12ca4a926098e311d080248a2f25
Mohamed Aziz Najjar <njourawebdev@gmail.com>
2022-10-01 20:06:50 +0200

    navBar+logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { HiMenuAlt4, HiX } from 'react-icons/hi'
+import { motion } from 'framer-motion'
 
 import { images } from '../../constants'
 import './Navbar.scss'
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false)
     return (
         <nav className="app__navbar">
             <div className="app__navbar-logo">
                 <img
-                    src={images.logo}
+                    className="LOGO"
+                    src={images.nwdLogo}
                     alt="logo"
                 />
             </div>
@@ -23,6 +27,32 @@ const Navbar = () => {
                     </li>
                 ))}
             </ul>
+            <div className="app__navbar-menu">
+                <HiMenuAlt4 onClick={() => setToggle(true)} />
+
+                {toggle && (
+                    <motion.div
+                        whileInView={{ x: [300, 0] }}
+                        transition={{ duration: 0.85, ease: 'easeOut' }}
+                    >
+                        <HiX onClick={() => setToggle(false)} />
+                        <ul>
+                            {['home', 'about', 'work', 'skills', 'contact'].map(
+                                (item) => (
+                                    <li key={item}>
+                                        <a
+                                            href={`#${item}`}
+                                            onClick={() => setToggle(false)}
+                                        >
+                                            {item}
+                                        </a>
+                                    </li>
+                                )
+                            )}
+                        </ul>
+                    </motion.div>
+                )}
+            </div>
         </nav>
     )
 }
