08bce62aa9857d47adbcad241d2d985338b5d2eb
Mohamed Aziz Najjar <njourawebdev@gmail.com>
2022-10-02 03:20:33 +0200

    header section

--- src/constants/images.js
+++ src/constants/images.js
@@ -15,6 +15,7 @@ import python from '../assets/python.png'
 import react from '../assets/react.png'
 import redux from '../assets/redux.png'
 import sass from '../assets/sass.png'
+import bootstrap from '../assets/bootstrap.png'
 import typescript from '../assets/typescript.png'
 import vue from '../assets/vue.png'
 
@@ -53,6 +54,7 @@ export default {
     react,
     redux,
     sass,
+    bootstrap,
     typescript,
     vue,
     about01,
